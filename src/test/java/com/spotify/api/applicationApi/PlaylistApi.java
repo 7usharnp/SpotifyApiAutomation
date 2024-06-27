318438e9ba0499ee9280167bcf5f70ed333f4646
Tushar.Nangare <tushar.nangare@hansencx.com>
2024-06-27 19:31:49 +0530

    Routes file added

--- src/test/java/com/spotify/api/applicationApi/PlaylistApi.java
+++ src/test/java/com/spotify/api/applicationApi/PlaylistApi.java
@@ -4,23 +4,25 @@ import com.spotify.api.RestResource;
 import com.spotify.pojo.Playlist;
 import io.restassured.response.Response;
 
+import static com.spotify.api.Routes.PLAYLISTS;
+import static com.spotify.api.Routes.USERS;
 import static com.spotify.api.TokenManager.getToken;
 
 public class PlaylistApi {
     static String token = "";
 
     public static Response post(Playlist requestPlaylist){
-        return RestResource.post("/users/c0sb51wjod0fli2g7k0r2lc5e/playlists",getToken(), requestPlaylist);
+        return RestResource.post(USERS+"/c0sb51wjod0fli2g7k0r2lc5e/playlists",getToken(), requestPlaylist);
 
     }
 
     public static Response get(String playlistId){
-        return RestResource.get("/playlists/"+playlistId, getToken());
+        return RestResource.get(PLAYLISTS+"/"+playlistId, getToken());
 
     }
 
     public static Response update(Playlist requestPlaylist,String playlistId){
-        return RestResource.update("/users/"+playlistId+"/playlists",getToken(), requestPlaylist);
+        return RestResource.update(USERS+"/"+playlistId+"/playlists",getToken(), requestPlaylist);
     }
 
 }
