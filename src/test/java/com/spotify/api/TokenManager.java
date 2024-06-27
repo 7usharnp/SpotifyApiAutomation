2e483321c8454b29cab7b30051ac1e9e11210cde
Tushar.Nangare <tushar.nangare@hansencx.com>
2024-06-27 10:53:30 +0530

    code optimization

--- src/test/java/com/spotify/api/TokenManager.java
+++ src/test/java/com/spotify/api/TokenManager.java
@@ -2,13 +2,36 @@ package com.spotify.api;
 
 import io.restassured.http.ContentType;
 import io.restassured.response.Response;
+
+import java.time.Instant;
 import java.util.HashMap;
 
 import static com.spotify.api.SpecBuilder.getResponseSpecification;
 import static io.restassured.RestAssured.given;
 
 public class TokenManager {
-    public static String renewToken(){
+
+    private static String access_token;
+    private static Instant expiry_time;
+
+    public static String getToken(){
+        try{
+            if(access_token == null || Instant.now().isAfter(expiry_time)){
+                System.out.println("Renewing token");
+                Response response = renewToken();
+                access_token = response.path("access_token");
+                expiry_time = Instant.now().plusSeconds(3600);
+
+            }else {
+                System.out.println("Using old token..");
+            }
+        }
+        catch (Exception e){
+            throw new RuntimeException("ABORT!! Failed to generate Token");
+        }
+        return access_token;
+    }
+    public static Response renewToken(){
         HashMap<String,String> formParam = new HashMap<String,String>();
         formParam.put("client_id","ed3f6c865f21439b97b3c3cdad22147e");
         formParam.put("client_secret","8339b7f9c91247d7893d0cad6556b1b8");
@@ -26,7 +49,7 @@ public class TokenManager {
        if(response.statusCode() !=200){
            throw new RuntimeException("ABORT !! Token creation failed......😒");
        }
-       return response.path("access_token");
+       return response;
     }
 
 
--- src/test/java/com/spotify/api/applicationApi/PlaylistApi.java
+++ src/test/java/com/spotify/api/applicationApi/PlaylistApi.java
@@ -1,27 +1,26 @@
 package com.spotify.api.applicationApi;
 
 import com.spotify.api.RestResource;
-import com.spotify.api.TokenManager;
 import com.spotify.pojo.Playlist;
 import io.restassured.response.Response;
 
-import static com.spotify.api.TokenManager.renewToken;
+import static com.spotify.api.TokenManager.getToken;
 
 public class PlaylistApi {
     static String token = "";
 
     public static Response post(Playlist requestPlaylist){
-        return RestResource.post("/users/c0sb51wjod0fli2g7k0r2lc5e/playlists",renewToken(), requestPlaylist);
+        return RestResource.post("/users/c0sb51wjod0fli2g7k0r2lc5e/playlists",getToken(), requestPlaylist);
 
     }
 
     public static Response get(String playlistId){
-        return RestResource.get("/playlists/"+playlistId, renewToken());
+        return RestResource.get("/playlists/"+playlistId, getToken());
 
     }
 
     public static Response update(Playlist requestPlaylist,String playlistId){
-        return RestResource.update("/users/"+playlistId+"/playlists",renewToken(), requestPlaylist);
+        return RestResource.update("/users/"+playlistId+"/playlists",getToken(), requestPlaylist);
     }
 
 }
